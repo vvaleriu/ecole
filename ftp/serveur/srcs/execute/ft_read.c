eb95770b63506769294439b0dd48e69189552bd0
vvaleriu <double_u_vince@hotmail.com>
2015-06-12 14:30:40 +0200

    save

--- ftp/serveur/srcs/execute/ft_read.c
+++ ftp/serveur/srcs/execute/ft_read.c
@@ -9,7 +9,7 @@ void		ft_read(t_sv_prop *sv, int i)
 {
 	while (get_next_line(CL_SOCK(i), &(sv->cmd->cmd)) > 0)
 	{
-		ft_printf(sv->cmd->cmd);
+	//	ft_printf(sv->cmd->cmd);
 		sv->cmd->cmda = lexer(sv->cmd->cmd);
 		if (check_command(sv))
 			exe_command(sv);
